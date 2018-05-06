9648b7dec5b32660d4257888f786538737c0e135
Daniel Wu <1danielbwu1@gmail.com>
2018-05-05 23:11:28 -0600

    Initial push for homepage

---
+++ web/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="css/home.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<!--Navbar-->
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Daniel Wu</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Assignments</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<!--Main content-->
+	<div class="col-sm-2">
+
+	</div>
+	<div class="jumbotron text-center">
+		<h1>Hello</h1>
+		<p>Hello there</p>
+		<img src="images/headshot.jpg" class="img-rounded img-responsive" alt="daniel wu" />
+	</div>
+	<div class="container-fluid text-center">
+		<p>Here's some text</p>
+	</div>
+</body>
+</html>
