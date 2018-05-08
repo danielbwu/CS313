8c53c4f1da6d1de69e97bd47400762d36385d025
Daniel Wu <1danielbwu1@gmail.com>
2018-05-07 19:54:45 -0600

    Working on homepage

--- web/home.php
+++ web/home.php
@@ -2,8 +2,9 @@
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <title>Homepage</title>
+   <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Homepage</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -24,22 +25,42 @@
 		<div class="col-sm-2"></div>
 
 		<div class="container-fluid col-sm-8">
-			<div class="jumbotron text-center row container-fluid bio">
-				<div class="col-sm-6">
-					<img src="images/headshot.jpg" class="img-rounded img-responsive" alt="daniel wu" />
+			<div class="jumbotron text-center row container-fluid" id="bio">
+				<div class="col-sm-6 container">
+					<img src="images/headshot.jpg" class="img-rounded img-responsive" alt="daniel wu" id="headshot" />
 				</div>
-				<div class="col-sm text-left">
+				<div class="col-sm text-left well">
 					<h2>Daniel Wu</h2>
 					<p>
+						<b>Hometown:</b><br />
+						Mesa, AZ<br />
 						<b>Major:</b><br />
-						Software Engineering
+						Software Engineering<br />
+						<b>Email:</b><br />
+						wu15002@byui.edu<br />
+						<b>Phone:</b><br />
+						123-456-7890<br />
 					</p>
 				</div>
 			</div>
 
-			<div class="container text-center">
-				<p>Here's some text</p>
+			<div class="container-fluid text-center col-sm-6" id="about">
+				<ul class="list-group">
+					<li class="list-group-item active"><b>About Me</b></li>
+					<li class="list-group-item">Hi! My name is Daniel Wu. I grew up in Mesa,
+					Arizona, and I currently live in Rexburg, Idaho attending Brigham Young
+					University - Idaho. I am a Software Engineering student in my 8th semester.</li>
+				</ul>
 			</div>
+
+			<div class="container-fluid text-left col-sm-6" id="hobbies">
+				<ul class="list-group">
+					<li class="list-group-item active"><b>Hobbies:</b></li>
+					<li class="list-group-item">Programming</li>
+					<li class="list-group-item">Computer Building</li>
+				</ul>
+			</div>
+
 		</div>
 
 		<div class="col-sm-2"></div>
--- web/nav.php
+++ web/nav.php
@@ -5,7 +5,7 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="#">Daniel Wu</a>
+			<span class="navbar-brand">Daniel Wu</span>
 		</div>
 		<ul class="nav navbar-nav">
 			<li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
