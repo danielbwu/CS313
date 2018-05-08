90f8d7c0e9822909a2c7acaadbe2de98fb809fde
Daniel Wu <1danielbwu1@gmail.com>
2018-05-07 22:01:31 -0600

    fixed bootstrap references

--- web/assignments.php
+++ web/assignments.php
@@ -2,9 +2,17 @@
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <title>Hello World</title>
+   <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Assignments</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<?php
