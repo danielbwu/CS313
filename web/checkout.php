46bd203058b96ecce9cc6ec20fb243e86d079302
Daniel Wu <1danielbwu1@gmail.com>
2018-05-15 03:05:13 -0600

    Fixed labels in checkout

--- web/checkout.php
+++ web/checkout.php
@@ -22,25 +22,27 @@
 	?>
     <!--Main Content-->
     <div class="container">
-        <form method="post" action="confirmation.php">
-            <div class="form-group">
-                <label for="streetAddress">Street address</label>
-                <input type="text" class="form-control" name="streetAddress" placeholder="Enter address" />
-            </div>
-            <div class="form-group">
-                <label for="city">City</label>
-                <input type="text" class="form-control" name="city" placeholder="Enter city" />
-            </div>
-            <div class="form-group">
-                <label for="state">State/Province</label>
-                <input type="text" class="form-control" name="state" placeholder="State/Province" />
-            </div>
-            <div class="form-group">
-                <label for="zip">Zip code</label>
-                <input type="number" class="form-control" name="zip" placeholder="Enter city" />
-            </div>
-            <input type="submit" class="btn btn-primary" value="Place Order" />
-        </form>
+        <div class="container-fluid col-md-6">
+            <form method="post" action="confirmation.php">
+                <div class="form-group">
+                    <label for="streetAddress">Street address</label>
+                    <input type="text" class="form-control" name="streetAddress" placeholder="Enter address" />
+                </div>
+                <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" name="city" placeholder="Enter city" />
+                </div>
+                <div class="form-group">
+                    <label for="state">State/Province</label>
+                    <input type="text" class="form-control" name="state" placeholder="Enter State/Province" />
+                </div>
+                <div class="form-group">
+                    <label for="zip">Zip code</label>
+                    <input type="number" size="8" class="form-control" name="zip" placeholder="Enter zip code" />
+                </div>
+                <input type="submit" class="btn btn-primary" value="Place Order" />
+            </form>
+        </div>
     </div>
 </body>
 </html>
