dde6f798d415eeb3c862011ee4019631a7547cba
Daniel Wu <1danielbwu1@gmail.com>
2018-05-14 23:49:23 -0600

    Added to cart page. Added ability to add to cart

--- web/add_to_cart.php
+++ web/add_to_cart.php
@@ -1,5 +1,8 @@
 <?php
+    session_start();
+
     $id = $qty = "";
+    $success = true;
 
     //Get data from request
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -7,19 +10,21 @@
         //Get product id
         if (empty($_POST["id"])) {
             echo "ID not found";
+            $success = false;
         }
         else {
             $id = test_input($_POST["id"]);
-            echo $id;
+            
         }
 
         //Get quantity
         if (empty($_POST["qty"])) {
             echo "Quantity not found";
+            $success = false;
         }
         else {
             $qty = test_input($_POST["qty"]);
-            echo $qty;
+            
         }
         
     }
@@ -30,5 +35,21 @@
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
+    }
+
+    //Add items to cart
+    if ($success) {
+        if (!isset($_SESSION["cart"])) {
+            $_SESSION["cart"] = array();
+        }
+
+        //Check if item is already in cart
+        if (array_key_exists($id, $_SESSION["cart"])) {
+            $_SESSION["cart"][$id]->qty += $qty;
+        }
+        else {
+            $_SESSION["cart"][$id] = $_SESSION["items"][$id];
+            $_SESSION["cart"][$id]->qty = $qty;
+        }
     }
 ?>
--- web/cart.php
+++ web/cart.php
@@ -10,5 +10,31 @@
     <?php
 		require("nav.php");
 	?>
+    <!--Main Content-->
+    <div class="container">
+        <div class="container-fluid col-md-12">
+            <h1>Your Cart</h1>
+            <table class="table table-responsive">
+                <thead>
+                    <th>Product</th>
+                    <th class="col-md-1">Price</th>
+                    <th class="col-md-1">Quantity</th>
+                    <th class="col-md-2"></th>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($_SESSION["cart"] as $p => $p_value) {
+                            echo "<tr>" . 
+                                "<td>" . $p_value->name . "</td>\n" . 
+                                "<td>$" . $p_value->price . "</td>\n" . 
+                                "<td>" .  $p_value->qty . "</td>\n" . 
+                                "<td>" . "<button type=\"button\" class=\"btn btn-danger\" onclick=\"\">Remove</button><td>\n" . 
+                                "</tr>\n";
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
