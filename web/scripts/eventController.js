858263835ea63c884842bbeb8e4019220031cb2d
Daniel Wu <1danielbwu1@gmail.com>
2018-06-12 01:01:26 -0600

    Testing API

--- web/scripts/eventController.js
+++ web/scripts/eventController.js
@@ -1,6 +1,10 @@
 var app = angular.module("StudentEvents", []); 
 app.controller("EventCtrl", function($scope, $http, EventService) {
     $scope.init = function () {
-        $scope.debug = EventService.getEvents();
+        EventService.getEvents()
+            .then(function (response) {
+                $scope.debug = response.data;
+                console.log(response.data);
+            });
     };
 });
