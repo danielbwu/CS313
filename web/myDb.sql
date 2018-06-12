3b829ecfb407233055555f29ab6a35712ddc32bb
Daniel Wu <1danielbwu1@gmail.com>
2018-06-12 00:32:36 -0600

    Initial DB access

---
+++ web/myDb.sql
@@ -0,0 +1,92 @@
+CREATE TABLE event
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    "description" VARCHAR(1000),
+    "date" date,
+    start_time TIME,
+    end_time TIME
+);
+
+INSERT INTO event(name, location, "description", "date", start_time, end_time) VALUES ('Barbeque', 'Porter Park', 'A good old-fashioned summer barbeque!', '2018-07-16', '12:00:00', '15:00:00');
+INSERT INTO event(name, location, "description", "date", start_time, end_time) VALUES ('Temple Activity', 'Rexburg Temple', 'We are doing baptisms for the dead. If you have family names, please contact (123) 456-7890.', '2018-07-15', '10:00:00', '12:00:00');
+INSERT INTO event(name, location, "description", "date", start_time, end_time) VALUES ('Movie Night', 'Crossroads', 'Star Wars marathon! Bring your friends!', '2018-07-15', '12:00:00', '22:00:00');
+INSERT INTO event(name, location, "description", "date", start_time, end_time) VALUES ('', '', '', '2018-07-15', '12:00:00', '22:00:00');
+
+CREATE TABLE participant
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    inumber VARCHAR(9) NOT NULL UNIQUE
+);
+
+INSERT INTO participant(name, inumber) VALUES ('Daniel Wu', '772362581');
+INSERT INTO participant(name, inumber) VALUES ('John Smith', '123456789');
+INSERT INTO participant(name, inumber) VALUES ('Mary Sue', '111111111');
+INSERT INTO participant(name, inumber) VALUES ('James Richards', '321654987');
+INSERT INTO participant(name, inumber) VALUES ('Frank West', '125976358');
+INSERT INTO participant(name, inumber) VALUES ('Jane Doe', '156487325');
+INSERT INTO participant(name, inumber) VALUES ('Edgar Jones', '467953816');
+
+CREATE TABLE event_participant
+(
+    id SERIAL PRIMARY KEY,
+    event_id INT REFERENCES event(id) NOT NULL,
+    participant_id INT REFERENCES participant(id) NOT NULL,
+    notes VARCHAR(1000)
+);
+
+CREATE TABLE tag
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE
+);
+
+INSERT INTO tag(name) VALUES ('temple');
+INSERT INTO tag(name) VALUES ('baptism');
+INSERT INTO tag(name) VALUES ('movie');
+INSERT INTO tag(name) VALUES ('bbq');
+INSERT INTO tag(name) VALUES ('food');
+INSERT INTO tag(name) VALUES ('game');
+INSERT INTO tag(name) VALUES ('dnd');
+INSERT INTO tag(name) VALUES ('outdoors');
+INSERT INTO tag(name) VALUES ('crossroads');
+
+CREATE TABLE event_tag
+(
+    id SERIAL PRIMARY KEY,
+    event_id INT REFERENCES event(id) NOT NULL,
+    tag_id INT REFERENCES tag(id) NOT NULL
+);
+
+
+-- CREATE TABLE registration
+-- (
+--     id SERIAL PRIMARY KEY,
+--     participant_id INT REFERENCES participant(id) NOT NULL,
+--     event_id INT REFERENCES event(id) NOT NULL,
+--     notes VARCHAR(1000)
+-- );
+
+-- CREATE TABLE game_table
+-- (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     "description" VARCHAR(1000),
+--     game_master_id INT REFERENCES participant(id)
+-- );
+
+-- CREATE TABLE game_table_participant
+-- (
+--     id SERIAL PRIMARY KEY,
+--     game_table_id INT REFERENCES game_table(id) NOT NULL,
+--     participant_id INT REFERENCES participant(id) NOT NULL
+-- );
+
+-- CREATE TABLE event_game_table
+-- (
+--     id SERIAL PRIMARY KEY,
+--     event_id INT REFERENCES event(id) NOT NULL,
+--     game_table_id INT REFERENCES game_table(id) NOT NULL
+-- );
