ddfb540214cf23c40469c76c726ceaed64c18d4f
Daniel Wu <1danielbwu1@gmail.com>
2018-06-12 00:56:07 -0600

    Changed request url

--- web/scripts/eventService.js
+++ web/scripts/eventService.js
@@ -1,7 +1,7 @@
 app.service('EventService', function($http) {
     //Gets all events
     this.getEvents = function () {
-        return $http.get("../api/get_events.php");
+        return $http.get("https://ancient-eyrie-30939.herokuapp.com/api/get_events.php");
         //return "Hello World!";
     }
 });
